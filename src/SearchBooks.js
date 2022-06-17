1ffe7b67a87055a9b7aad32d057548a357d9585b
ahmed elsayed <ahmedelsayed3157@gmail.com>
2022-06-17 23:27:40 +0200

    add debounce for search page

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,7 +1,9 @@
-import React , {useState ,useEffect}  from 'react' ;
+import React , {useState ,useEffect, useMemo}  from 'react' ;
 import { Link } from "react-router-dom";
 import Results from './Results';
 import * as Api from './BooksAPI' ;
+import { debounce } from 'lodash'
+
 
 
 const SearchBooks = (props) => {
@@ -9,10 +11,23 @@ const SearchBooks = (props) => {
   const [q , setQ] = useState("");
   const [books , setBooks] = useState([]);
   
+
+
+  const getBooks = () => useMemo(
+    () => {
+      debounce(() => {
+      console.log(q);
+      Api.search(q , 10).then(a => {a.error ? setBooks([]) : setBooks(a) }) ;
+      } , 100) ;
+    },
+    [q],
+  )
+  
   useEffect(() => {
    
     if(q.trim() !== '') {
-    Api.search(q , 10).then(a => {a.error ? setBooks([]) : setBooks(a) }) ;
+      console.log('effect: ' , q);
+      getBooks();
     }else {
       setBooks([]) ;
     }
@@ -32,6 +47,7 @@ const SearchBooks = (props) => {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
+                
                 <input type="text" value={q} onChange={(e) => setQ(e.target.value)} placeholder="Search by title or author"/>
 
               </div>
